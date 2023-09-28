a948d819a9f7086fc793989f9c0df3eb64d180ab
Ahmed_Mansour <78514942+Up-to-code@users.noreply.github.com>
2023-09-28 15:54:21 +0300

    test

--- app/db/db.js
+++ app/db/db.js
@@ -2,22 +2,22 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import "dotenv/config"
 /*
- = AIzaSyCBcfSxKljnhvsFNu8tblmWDeXiyir_urE
- = the-schoole.firebaseapp.com
- = the-schoole
- = the-schoole.appspot.com
- = 22940945203
- = 1:22940945203:web:48e1fb8752aa1aa7b7cc46
-
-*/
-
-const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DPMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
+
+*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBcfSxKljnhvsFNu8tblmWDeXiyir_urE",
+  authDomain: "the-schoole.firebaseapp.com",
+  projectId: "the-schoole",
+  storageBucket: "the-schoole.appspot.com",
+  messagingSenderId: "22940945203",
+  appId: "1:22940945203:web:48e1fb8752aa1aa7b7cc46"
 };
 
 // Initialize Firebase
