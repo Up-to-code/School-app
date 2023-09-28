beba4701915b44ff68b36f2eb5aca5f073889de8
Ahmed_Mansour <78514942+Up-to-code@users.noreply.github.com>
2023-09-28 15:45:55 +0300

    t

--- app/context/User_Deta.jsx
+++ app/context/User_Deta.jsx
@@ -1,26 +1,16 @@
 "use client"
 import { createContext, useState } from "react";
 import {  onAuthStateChanged } from "firebase/auth";
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Auth } from "../db/db";
 
 export const UserContext = createContext()
 
 
 function User_Deta({children}) {
- const [user ,setUser] = useState()    
 
-onAuthStateChanged(Auth, (user) => {
-   
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      const USER = user;
-      setUser(USER)
+ const [user, loading, error] = useAuthState(Auth);
 
-    } else {
-
-    }
-  });
 
    
   return (
