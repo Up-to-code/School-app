a91d6b7a7e6f7adcfc9e46dfab670e0abe1ac8e6
Ahmed_Mansour <78514942+Up-to-code@users.noreply.github.com>
2023-09-28 16:09:56 +0300

    h

--- app/components/Google/Button_Auth_Google.jsx
+++ app/components/Google/Button_Auth_Google.jsx
@@ -9,7 +9,7 @@ function Button_Auth_Google() {
   const HdailGoogleAuth = async () => {
     try {
       const result = await signInWithPopup(Auth, provider);
-      console.log(result.user)
+     
     } catch (error) {
       console.log(error);
     }
